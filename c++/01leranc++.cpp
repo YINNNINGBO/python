ebc26c048e7ff208f846cfa87a8209cdd6c7c7fa
Peter.Ningbo <1153671643@qq.com>
2024-07-24 17:11:55 +0800

    ~stock

--- c++/01leranc++.cpp
+++ c++/01leranc++.cpp
@@ -6,17 +6,21 @@ class stock{
 		int  m1;
 	public:
 		stock(void){
-		//	int m1;
-			cout<<"stock(void)"<<endl;
+			int m1 = 111;
+			cout<<"stock(void)"<< " "<<m1<<endl;
 		}
 		stock(int n){
 			m1 = n;
 			cout<< "stock(int n)"<<m1<<endl;
 		}
+		~stock(void ){
+			cout << "~stock "<< endl;
+		}
 };
 int main(void)
 {
 	stock mm;
+	stock m2(3);
 	return 0;
 
 }
