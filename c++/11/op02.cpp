631f41e7a7f876a28ab47aa047dae736729a6683
Peter.Ningbo <1153671643@qq.com>
2024-07-31 15:44:53 +0800

    operatir <运算符> 重载

--- c++/11/op02.cpp
+++ c++/11/op02.cpp
@@ -36,21 +36,33 @@ public:
         cout << "b_t = " << b_t << ", b_y = " << b_f << endl;
     }
 	 // 声明友元函数，用于访问 B 类的私有成员
-    friend B operator!( B& b);
+	 //这个函数返回值不是引用  返回赋值体 ， 原b 不改变
+	 //******'!' 是不改变原数值的**
+	friend B operator!( B& b)
+		// 友元函数重载逻辑非操作符 !
+		 {
+			return B(!(b.b_t), !(b.b_f));
+		}
+		/*   这个是返回值是 &  原b 发生改变
+	friend B& operator!(B& b) {
+        b.b_t = !(b.b_t);
+        b.b_f = !(b.b_f);
+        return b;
+    }
+*/
 
 };
 
-// 友元函数重载逻辑非操作符 !
-B operator!( B& b) {
-    return B(!(b.b_t), !(b.b_f));
-}
-
 int main() {
     B *b1 = new B(2, 3);
+	cout << "b1.b_t = " << b1->getx() << ", b1.b_f = " << b1->gety() << endl;
+
     A a = !(*b1);  // 使用 operator!() 返回的 A 对象
 
     cout << "After logical NOT operation:" << endl;
-    cout << "a.b_t = " << a.getx() << ", a.b_f = " << a.gety() << endl;
+    cout << "b1.b_t = " << b1->getx() << ", b1.b_f = " << b1->gety() << endl;
+
+	cout << "a.b_t = " << a.getx() << ", a.b_f = " << a.gety() << endl;
     
     return 0;
 }
