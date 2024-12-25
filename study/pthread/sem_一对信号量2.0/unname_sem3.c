6e19cb4550fa402f3d854e622cc3de094b87ea08
Peter.Ningbo <1153671643@qq.com>
2024-12-26 03:06:45 +0800

    信号量同步,与线程交替调用难点

--- study/pthread/sem_一对信号量2.0/unname_sem3.c
+++ study/pthread/sem_一对信号量2.0/unname_sem3.c
@@ -2,32 +2,21 @@
 #include "user.h"
 
 #define BUF_SIZE 5 // 缓冲区大小
-/***********生产者与消费者线程中 延时函数相同时  可以做到交替输出*****
- * **********两个线程在解锁后使用不同的  usleep：
- *
-        生产者生产数据: 0
+/***********生产者与消费者线程中 延时函数相同时  可以做到交替输出******
+ *  * 相比unname_sem.c   改变 sem_init(&empty, 0, BUF_SIZE);  中第三个参数大小
+ *  只有当为1 时  无论  usleep如何调整都是：
+    ./unname_sem3
+    生产者生产数据: 0
     消费者消费数据: 0
-        生产者生产数据: 1
+    生产者生产数据: 1
     消费者消费数据: 1
-        生产者生产数据: 2
-        生产者生产数据: 3
+    生产者生产数据: 2
     消费者消费数据: 2
-        生产者生产数据: 4
+    生产者生产数据: 3
     消费者消费数据: 3
+    生产者生产数据: 4
     消费者消费数据: 4
-***********两个线程使用相同的  usleep：
-        生产者生产数据: 0
-    消费者消费数据: 0
-       生产者生产数据: 1
-    消费者消费数据: 1
-        生产者生产数据: 2
-    消费者消费数据: 2
-        生产者生产数据: 3
-    消费者消费数据: 3
-        生产者生产数据: 4
-    消费者消费数据: 4
-*/
-
+ */
 // 共享缓冲区
 int buffer[BUF_SIZE];
 int in = 0;  // 写入位置
@@ -48,11 +37,10 @@ void *producer(void *arg)
 
         buffer[in] = i; // 写入数据
         printf("生产者生产数据: %d\n", i);
-        in = (in + 1) % BUF_SIZE; // 更新写入位置
-
+        in = (in + 1) % BUF_SIZE;     // 更新写入位置
+        usleep(500000);               // usleep(500000);               // 模拟生产的延迟
         pthread_mutex_unlock(&mutex); // 离开临界区
         sem_post(&full);              // 增加满的位置，通知消费者
-        usleep(500000);               // 模拟生产的延迟
     }
     return NULL;
 }
@@ -63,14 +51,13 @@ void *consumer(void *arg)
     {
         sem_wait(&full);            // 等待直到缓冲区有数据
         pthread_mutex_lock(&mutex); // 进入临界区
-
-        int data = buffer[out]; // 读取数据
+        usleep(900000);             //  usleep(500000);               // 模拟消费的延迟
+        int data = buffer[out];     // 读取数据
         printf("消费者消费数据: %d\n", data);
         out = (out + 1) % BUF_SIZE; // 更新读取位置
 
         pthread_mutex_unlock(&mutex); // 离开临界区
         sem_post(&empty);             // 增加空的位置，通知生产者
-        usleep(800000);               // 模拟消费的延迟
     }
     return NULL;
 }
@@ -78,7 +65,7 @@ void *consumer(void *arg)
 int main()
 {
     // 初始化信号量
-    sem_init(&empty, 0, BUF_SIZE);    // 初始时缓冲区是空的
+    sem_init(&empty, 0, 1);           // 初始时缓冲区是空的
     sem_init(&full, 0, 0);            // 初始时没有数据
     pthread_mutex_init(&mutex, NULL); // 初始化互斥锁
 
