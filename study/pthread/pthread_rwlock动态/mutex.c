2fe401bfd5a1f8bf827fb69958d10cd13e487f76
Peter.Ningbo <1153671643@qq.com>
2024-12-31 15:11:21 +0800

    注释修改

--- study/pthread/pthread_rwlock动态/mutex.c
+++ study/pthread/pthread_rwlock动态/mutex.c
@@ -13,14 +13,14 @@ void *mutex_read(void *arg)
 {
     /**
      * 读  分上锁与不上锁
-     * 上锁    会一次次读，每次读取中间隔 100000um  显然这一种毫无意义
-     * 不上锁  会同时读  ，每次读取中间隔 100000um
+     * 上锁    并行读，但是不回被其他写线程打断
+     * 不上锁 并行读 但是又可能被写线程打断
      */
     // 上读锁  不让其他线程写，同使可以多个线程并发读
-    pthread_rwlock_rdlock(&rwlock);
+   // pthread_rwlock_rdlock(&rwlock);
     printf("在<读>线程内data:>%d,arg:>%s\n", data, (char *)arg);
-    usleep(500000); // 可以看出上锁后以一次次读还是 一次去全读
-    pthread_rwlock_unlock(&rwlock);
+    sleep(1); // 可以看出上锁后以一次次读还是 一次去全读
+   // pthread_rwlock_unlock(&rwlock);
 }
 void *mutex_write(void *arg)
 {
@@ -46,7 +46,7 @@ int main(int argc, const char **argv)
     pthread_create(&tid_r0, NULL, mutex_read, (void *)"read1");
     pthread_create(&tid_r1, NULL, mutex_read, (void *)"read2");
     pthread_create(&tid_r2, NULL, mutex_read, (void *)"read3");
-    sleep(2);
+    sleep(4);
     /**
      * 这个时间必须 >  各线程都执行完后相加的时间 ，
      * 否则父进程结束，旗下的线程即便使用“pthread_detach”  分离  也会因为父进程结束而结束
