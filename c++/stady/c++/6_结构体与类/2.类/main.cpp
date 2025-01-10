61fa9266e992b0b3f7e0d11f34e1bbe66baa49e2
Peter.Ningbo <1153671643@qq.com>
2025-01-11 00:13:58 +0800

    c++下的结构体与类

---
+++ c++/stady/c++/6_结构体与类/2.类/main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+/*               类中不使用关键字（public..）标注变量，函数的化，成员默认是私有的    */
+
+/*                       类                         */
+class Complex
+{
+    // 默认私有
+    double r; // 实部
+    double i; // 虚部
+
+    // 函数公有
+public:
+    void init(double rr, double ii)
+    {
+        r = rr;
+        i = ii;
+    }
+    double real()
+    {
+        return r;
+    }
+    double image()
+    {
+        return i;
+    }
+    void set_r(double rr)
+    {
+        r = rr;
+    }
+};
+int main()
+{
+    Complex a; // 定义一个复数结构
+    a.init(2, 3);
+    cout << a.real() << " + " << a.image() << "i" << endl;
+    // a.r = 4; // 这里默认是私有的 ‘double Complex::r’ is private within this context
+    return 0;
+}
+/**
+ *
+g++ main.cpp -c  -o main.o
+g++ main.o -o main
+./main
+2 + 3i
+ */
